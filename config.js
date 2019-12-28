9481fbc1af259dd02c43de931ae7ad991c2162ae
mer1dian <sambacha@gmail.com>
2019-12-28 14:07:16 -0800

    intial

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "Freight Trust Network Status Page",
+  "name": "freight_trust_network_status_page",
+  "description": "freight trust blockchain network and platform uptime dashboard",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
